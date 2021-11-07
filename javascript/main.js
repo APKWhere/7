d18e5a30f237eec4f3331510150d7fdb9d43d5fc
APKWhere <93852176+APKWhere@users.noreply.github.com>
2021-11-07 14:55:04 +0530

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,3 @@
 /*
 Lol, nothing here. By the way, thanks for opening this file.
-This is an testing site, here you can do anything. Like, cloning
-this website, testing your tools on this website, but don't forget
-that this website is as it is a GitHub Page, hosted on GitHub. You
-know, what GitHub is. Thanks again, follow github guidelines then
-start your all tests on this Website.
 */
